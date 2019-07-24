a4f983839d5a562c715bd463741bd06eaa741179
pengjiyuan <pengjiyuan@bytedance.com>
2019-07-24 11:20:54 +0800

    feat: support server render

--- src/raf.js
+++ src/raf.js
@@ -1,31 +1,34 @@
-const vendors = ['webkit', 'ms', 'moz', 'o']
+const vendors = ['webkit', 'ms', 'moz', 'o'];
 
-let raf = window && window.requestAnimationFrame;
-let caf = window && window.cancelAnimationFrame;
+const isServerRender = !(typeof window !== undefined && document !== undefined);
 
-if (window && (!raf || !caf)) {
-  vendors.some(prefix => {
-    raf = window[`${prefix}RequestAnimationFrame`]
-    caf = window[`${prefix}CancelAnimationFrame`] || window[`${prefix}CancelRequestAnimationFrame`]
-    return raf && caf
-  })
-
+if (!isServerRender) {
+  let raf = window && window.requestAnimationFrame;
+  let caf = window && window.cancelAnimationFrame;
   if (!raf || !caf) {
-    let lastTime = 0
-
-    raf = function (cb) {
-      const now = Date.now()
-      const diff = now - lastTime
-      lastTime = now
-      // 每秒至少60帧
-      return setTimeout(cb, Math.min(0, 1000 / 60))
+    vendors.some(prefix => {
+      raf = window[`${prefix}RequestAnimationFrame`]
+      caf = window[`${prefix}CancelAnimationFrame`] || window[`${prefix}CancelRequestAnimationFrame`]
+      return raf && caf
+    })
+  
+    if (!raf || !caf) {
+      let lastTime = 0
+  
+      raf = function (cb) {
+        const now = Date.now()
+        const diff = now - lastTime
+        lastTime = now
+        // 每秒至少60帧
+        return setTimeout(cb, Math.min(0, 1000 / 60))
+      }
+  
+      caf = function (timer) {
+        clearTimeout(timer)
+      }
     }
-
-    caf = function (timer) {
-      clearTimeout(timer)
-    }
-  }
-
-  window.requestAnimationFrame = raf
-  window.cancelAnimationFrame = caf
+  
+    window.requestAnimationFrame = raf
+    window.cancelAnimationFrame = caf
+  }  
 }
